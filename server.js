eafecf548b201a28d5095334225dab02e422bb67
bez <arapi.besmir@gmail.com>
2021-12-04 16:02:05 +0100

    Init client part

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const { dbConnect } = require('./db');
 
 const express = require('express');
 const cors = require('cors');
+const path = require("path")
 const dotenv = require('dotenv');
 dotenv.config();
 
@@ -12,6 +13,14 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+if (process.env.NODE_ENV !== "development") {
+  app.use(express.static(path.join(__dirname, "client", "build")));
+  
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  });
+}
 
 app.use('/api/v1/tasks', tasksRoutes);
 app.use('/api/v1/users', usersRoutes);
